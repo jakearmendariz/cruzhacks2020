c879d1aa74a067f6e4134d9df45c632ccecdb3cc
sadschmi <sadschmi@ucsc.edu>
2020-01-18 01:51:38 -0800

    added basic configuration file

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://readAndWrite:uJkfM1V7bi3xT13k@cluster0-ksxj2.gcp.mongodb.net/CharityDB?retryWrites=true&w=majority"
+app.config["MONGO_DBNAME"] = 'CharityDB'
+mongo = PyMongo(app)
+
+mongo.db.user.insert_one({
+    'name' : 'Sam',
+    'happiness' : 'nan'
+});
+
+
+@app.route("/")
+def home_page():
+    return 'Hello World!'
