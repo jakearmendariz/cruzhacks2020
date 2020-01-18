f1e9dc0707ba55b81177016f257c295ac8109bfa
sadschmi <sadschmi@ucsc.edu>
2020-01-18 15:56:53 -0800

    attempting to fix issues with getting form info

--- backend/views.py
+++ backend/views.py
@@ -4,18 +4,7 @@ from app import app
 from models import *
 from flask import Flask, render_template
 
-# have the mother fucking frontend redirect to this url and the get inserted into the db
-@app.route('/<string:page_name>/')
-def render_static(page_name):
-    return render_template('%s.html' % page_name)
- 
- 
-#@app.route('/')
-#def index():
-#    return '../templates/index.html'
-
-
-@app.route('/signup', methods=['POST'])
+@app.route('/result', methods=['POST'])
 def createUser():
     name = request.form['name']
     username = request.form['username']
@@ -24,3 +13,18 @@ def createUser():
     user.create()
     print("FUCK SOCIETY AND THEIR GODDAM RULES")
     return name
+
+# have the mother fucking frontend redirect to this url and the get inserted into the db
+@app.route('/<string:page_name>/', methods=['GET'])
+def render_static(page_name):
+    return render_template('%s.html' % page_name)
+ 
+@app.route('/')
+def index():
+    return render_template('index.html')
+ 
+#@app.route('/')
+#def index():
+#    return '../templates/index.html'
+
+
