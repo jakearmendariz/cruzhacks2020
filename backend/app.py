37a79fdbd5a5b2fbb3ac02443453d9c78ca0e7af
Samuel Schmidt <sadschmi@ucsc.edu>
2020-01-18 02:59:56 -0800

    exported configuring the mongodb uri to environment variables

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,13 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
+from os import environ
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://readAndWrite:uJkfM1V7bi3xT13k@cluster0-ksxj2.gcp.mongodb.net/CharityDB?retryWrites=true&w=majority"
-app.config["MONGO_DBNAME"] = 'CharityDB'
+
+app.config.from_envvar('APP_SETTINGS')
+
 mongo = PyMongo(app)
 
-mongo.db.user.insert_one({
-    'name' : 'Sam',
-    'happiness' : 'nan'
-});
-
-
-@app.route("/")
-def home_page():
-    return render_template("index.html")
+@app.route('/')
+def greeter():
+    return "Hello World!"
