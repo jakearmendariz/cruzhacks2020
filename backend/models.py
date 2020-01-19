a0a766c1065e564b2d2059a9909723205e77b99f
Jake Armendariz <jakearmendariz99@gmail.com>
2020-01-18 23:54:33 -0800

    Can now add charities

--- backend/models.py
+++ backend/models.py
@@ -1,43 +1,57 @@
 from app import mongo
 from hashlib import sha224
 
-class User():
-    """
-    Basic user object
-    """
+from util import *
+
+class Charity(dict):
     id = ""
     name = ""
     email = ""
-    passwordHash = ""
-
+    password = ""
+    description = ""
+    #img 
+    website = ""
+    
     def __init__(self,
                  name,
                  email,
-                 passwordHash):
+                 website,
+                 description,
+                 passwordHash):#Add in image later
 
-        self.id = sha224(email).hexdigest() 
+        self.id = sha224(email.encode('utf-8')).hexdigest() 
         self.name = name
         self.email = email
-        self.passwordHash = passwordHash
-
-    # create a new user from a dictionary
+        self.website = website
+        self.description = description
+        #self.img = img
+        self.password = hash_password(passwordHash)
+    
+      # create a new user from a dictionary
     @staticmethod
     def createFromDict(dict):
-        user = User(dict['name'],
+        charity = Charity(
+                    dict['name'],
                     dict['username'],
+                    dict['website'],
+                    dict['description'],
                     dict['pass'])
-        return user
+                    #dict['img'])
+        return charity
 
     # simply return the response of the created login
     def dbInsert(self):
-        return mongo.db.User.insert_one({
-            'id': self.id,
+        return mongo.db.Charity.insert_one({
+            '_id': self.id,
             'name': self.name,
             'email': self.email,
-            'passwordHash': self.passwordHash
+            'website':self.website,
+            'description':self.description,
+            'passwordHash': self.password
+            #'img':self.img
         })
-
-    # read in the response
+    
+     # read in the response
     def dbRead(self):
         # TODO: Samuel Schmidt implement this for logging in        
         pass
@@ -49,5 +63,60 @@ class User():
     # delete a user
     def dbDelete(self):
         pass
+ 
 
 
+class User(dict):
+    """
+    Basic user object
+    """
+    id = ""
+    name = ""
+    email = ""
+    passwordHash = ""
+
+    def __init__(self,
+                 name,
+                 email,
+                 password):
+
+        self.id = sha224(email.encode('utf-8')).hexdigest() 
+        self.name = name
+        self.email = email
+        self.passwordHash = hash_password(password)
+
+    # create a new user from a dictionary
+    @staticmethod
+    def createFromDict(dict):
+        user = User(dict['name'],
+                    dict['username'],
+                    dict['pass'])
+        return user
+
+    # signup a user
+    @staticmethod
+    def signup(dict):
+        user = User.createFromDict(dict)
+        user.dbInsert()
+    
+
+    # simply return the response of the created login
+    def dbInsert(self):
+        return mongo.db.User.insert_one({
+            "_id": self.id,
+            'name': self.name,
+            'email': self.email,
+            'passwordHash': (self.passwordHash).encode('utf-8')
+        })
+
+    # read in the response 
+    def dbRead(self):
+        pass
+
+    # update the existing user
+    def dbUpdate(self):
+        pass
+    
+    # delete a user
+    def dbDelete(self):
+        pass
--- backend/views.py
+++ backend/views.py
@@ -1,9 +1,9 @@
 # views file where we control the views
 
-from app import app
+from app import app, mongo
 from models import *
 from forms import *
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextField, SubmitField
@@ -18,17 +18,49 @@ from wtforms.validators import DataRequired, Length
 #    user.create()
 #    return render_template("result.html")
 
+
+@app.route('/charity/<string:name>')
+def displayCharity(name):
+    cursor = mongo.db.Charity.find_one_or_404({'name': name})
+    
+    #nameis = cursor.name
+   # email = cursor.email
+    #return 'my name is ' + nameis + ", " + email
+    return f'''
+        <h1>{cursor['name']}</h1>
+        <p>{cursor['email']}</p>
+        <p>{cursor['website']}</p>
+        <p>Who are we?<br>{cursor['Description']}</p>
+    '''
+
+
 @app.route('/<string:page_name>/', methods=['POST'])
 def renderPosts(page_name):
+    if page_name == 'addSuccess':
 
-    if page_name == 'signupSuccess':
         result = request.form
-        user = User.createFromDict(result)
-        user.dbInsert()
-        # TODO: add session information
-        return render_template("index.html")
+        charity = Charity(result.get('name'),
+                          result.get('email'),
+                          result.get('website'),
+                          result.get('description'),
+                          result.get('pass'))
+        charity.dbInsert()
+        return redirect(url_for('index'))
+    if page_name == 'signupSuccess':#usersignup
+        try:
+            result = request.form
+            User.signup(result)
+            return redirect(url_for('index'))
+        except:
+            return redirect(url_for('signupError'))
+   # el
 
     return render_template('%s.html' % page_name)
+
+
+@app.route('/signupError')
+def signupError():
+    return render_template('signupError.html')
 
 @app.route('/<string:page_name>/', methods=['GET'])
 def renderGets(page_name):
@@ -37,5 +69,3 @@ def renderGets(page_name):
 @app.route('/')
 def index():
     return render_template("index.html") 
-
-
