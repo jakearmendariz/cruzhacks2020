f8e69a35814ecf090cc821abca4c5d4f952b7a4f
rishikapatel <rishikanpatel@yahoo.com>
2020-01-19 02:15:21 -0800

    added frontend

--- backend/models.py
+++ backend/models.py
@@ -1,78 +1,9 @@
 from app import mongo
-from hashlib import sha224
 
-from util import *
-
-import json
-
-class Charity(object):
-    id = ""
-    name = ""
-    email = ""
-    password = ""
-    description = ""
-    #img 
-    website = ""
-    
-    def __init__(self,
-                 name,
-                 email,
-                 website,
-                 description,
-                 passwordHash):#Add in image later
-
-        self.id = sha224(email.encode('utf-8')).hexdigest() 
-        self.name = name
-        self.email = email
-        self.website = website
-        self.description = description
-        #self.img = img
-        self.password = hash_password(passwordHash)
-    
-      # create a new user from a dictionary
-    @staticmethod
-    def createFromDict(dict):
-        charity = Charity(
-                    dict['name'],
-                    dict['username'],
-                    dict['website'],
-                    dict['description'],
-                    dict['pass'])
-                    #dict['img'])
-        return charity
-
-    # simply return the response of the created login
-    def dbInsert(self):
-        return mongo.db.Charity.insert_one({
-            '_id': self.id,
-            'name': self.name,
-            'email': self.email,
-            'website':self.website,
-            'description':self.description,
-            'passwordHash': self.password
-            #'img':self.img
-        })
-    
-     # read in the response
-    def dbRead(self):
-        # TODO: Samuel Schmidt implement this for logging in        
-        pass
-
-    # update the existing user
-    def dbUpdate(self):
-        pass
-    
-    # delete a user
-    def dbDelete(self):
-        pass
- 
-
-
-class User(object):
+class User(dict):
     """
     Basic user object
     """
-    id = ""
     name = ""
     email = ""
     passwordHash = ""
@@ -80,56 +11,28 @@ class User(object):
     def __init__(self,
                  name,
                  email,
-                 password):
+                 passwordHash):
 
-        self.id = sha224(email.encode('utf-8')).hexdigest() 
         self.name = name
         self.email = email
-        self.passwordHash = hash_password(password)
-
-    # create a new user from a dictionary
-    @staticmethod
-    def createFromDict(dict):
-        user = User(dict['name'],
-                    dict['username'],
-                    dict['pass'])
-        return user
-
-    # signup a user
-    @staticmethod
-    def signup(dict):
-        user = User.createFromDict(dict)
-        return user.dbInsert()
-
-    @staticmethod
-    def login(dict):
-        user = User("",
-                    dict.get('username'),
-                    dict.get('pass'))
-        userDict = user.dbRead()
-        if not verify_password(userDict['passwordHash'], dict.get('pass')):
-            raise Exception("Invalid password")
-        return userDict
-    
+        self.passwordHash = passwordHash
 
     # simply return the response of the created login
-    def dbInsert(self):
+    def create(self):
         return mongo.db.User.insert_one({
-            "_id": self.id,
             'name': self.name,
             'email': self.email,
-            'passwordHash': (self.passwordHash)
+            'passwordHash': self.passwordHash
         })
 
     # read in the response 
-    def dbRead(self):
-        document = mongo.db.User.find_one({"_id": self.id})
-        return document
+    def read(self):
+        pass
 
     # update the existing user
-    def dbUpdate(self):
+    def update(self):
         pass
     
     # delete a user
-    def dbDelete(self):
+    def delete(self):
         pass
--- backend/views.py
+++ backend/views.py
@@ -1,91 +1,25 @@
 # views file where we control the views
 
-from app import app, mongo
+from app import app
 from models import *
-from forms import *
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template
 
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SubmitField
-from wtforms.validators import DataRequired, Length
+@app.route('/result', methods=['POST'])
+def createUser():
+    name = request.form['name']
+    username = request.form['username']
+    password = request.form['password']
+    user = User(name, username, password)
+    user.create()
+    print("FUCK SOCIETY AND THEIR GODDAM RULES")
+    return name
 
-from pymongo import errors
-
-#@app.route('/result', methods=['POST'])
-#def createUser():
-#    name = request.form['name']
-#    username = request.form['username']
-#    password = request.form['password']
-#    user = User(name, username, password)
-#    user.create()
-#    return render_template("result.html")
-
-
-@app.route('/charity/<string:name>')
-def displayCharity(name):
-    name = name.replace("+", " ")
-    cursor = mongo.db.Charity.find_one_or_404({'name': name})
-    name = name.title()
-    return f'''<h1>{name}</h1>
-                <h3>Find us at:{cursor['website']}</h3>
-                <p>{cursor['description']}</p>
-                <p>If you want to get involved. Email us at {cursor['email']}</p>
-                '''
-#    return f'''
-#        <h1>{cursor['name']}</h1>
-#        <p>{cursor['email']}</p>
-#        <p>{cursor['website']}</p>
-#        <p>Who are we?<br>{cursor['Description']}</p>
-#    '''
-
-
-@app.route('/<string:page_name>/', methods=['POST'])
-def renderPosts(page_name):
-    if page_name == 'addSuccess':
-        try:
-            result = request.form
-            charity = Charity(result.get('name').lower(),
-                          result.get('email'),
-                          result.get('website'),
-                          result.get('description'),
-                          result.get('pass')),
-            charity.dbInsert()
-            return redirect(url_for('index'))
-        except errors.DuplicateKeyError:
-            return render_template('addCharity.html', exception = "Email already in use")
-
-    if page_name == 'signupSuccess':
-        try:
-            result = request.form
-            User.signup(result)
-            return redirect(url_for('index'))
-        except errors.DuplicateKeyError:
-            return render_template('signup.html', exception = "Email already in use")
-    if page_name == 'loginSuccess':
-        try:
-            result = request.form
-            User.login(result)
-            return redirect(url_for("index"))
-        except TypeError as issue:
-            return render_template("login.html", exception = "User does not exist")
-        except Exception as issue:
-            return render_template("login.html", exception = issue)
-        except:
-            return render_template("loginError".html, exception = "I don't know")
-    return render_template('%s.html' % page_name) 
-
-@app.route('/loginError')
-def loginError():
-    return render_template('loginError.html')
-
-@app.route('/signupError')
-def signupError():
-    return render_template('signupError.html')
-
+# have the mother fucking frontend redirect to this url and the get inserted into the db
 @app.route('/<string:page_name>/', methods=['GET'])
-def renderGets(page_name):
+def render_static(page_name):
     return render_template('%s.html' % page_name)
  
 @app.route('/')
 def index():
-    return render_template("index.html") 
+    return render_template('index.html')
+  
